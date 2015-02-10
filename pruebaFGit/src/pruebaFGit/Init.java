06398a418a1a770fa7de1ff4f42f483ec02956e8
Octavio <Octavio@octavio-pc>
2015-02-09 21:01:13 -0300

    para Na

---
+++ pruebaFGit/src/pruebaFGit/Init.java
@@ -0,0 +1,10 @@
+package pruebaFGit;
+
+public class Init {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated 
+
+	}
+
+}
---
+++ pruebaFGit/src/pruebaFGit/Main.java
@@ -0,0 +1,7 @@
+package pruebaFGit;
+
+public class Main {
+	
+	
+
+}
